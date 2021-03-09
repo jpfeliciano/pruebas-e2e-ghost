16cbf2b9ea8f9192a900ce80a2e95adc041c7400
Juan Feliciano <jp.feliciano10@uniandes.edu.co>
2021-03-08 22:58:59 -0500

    Se muestran en la consola los comandos utilizados para ejecutar las pruebas.

--- regresion-resemble-cypress/execute-regresion.sh
+++ regresion-resemble-cypress/execute-regresion.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo -e "**** Ejecución de pruebas E2E en la versión 3.3.0 de Ghost **** \n"
+echo -e "\n**** Ejecución de pruebas E2E en la versión 3.3.0 de Ghost ****\n"
+echo -e "./node_modules/.bin/cypress run -C cypress-3.3.0.json -b chrome\n"
 ./node_modules/.bin/cypress run -C cypress-3.3.0.json -b chrome
 
-echo -e "**** Ejecución de pruebas E2E en la versión 3.41.1 de Ghost **** \n"
+echo -e "\n**** Ejecución de pruebas E2E en la versión 3.41.1 de Ghost ****\n"
+echo -e "./node_modules/.bin/cypress run -C cypress.json -b chrome \n"
 ./node_modules/.bin/cypress run -C cypress.json -b chrome
 
-echo "**** Análisis de resultados con ResembleJS **** \n"
-node main.js
+echo -e "\n**** Análisis de resultados con ResembleJS **** \n"
+echo -e "node main.js\n"
+##node main.js
