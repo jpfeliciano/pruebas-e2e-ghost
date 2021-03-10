ebcf00818cf304c7d4584ba27ba0481f8fa9591f
hector-arias <h.arias@uniandes.edu.co>
2021-03-09 21:15:03 -0500

    Add files via upload

---
+++ ghost-cypress-test/cypress/integration/13_list_post_success.spec.js
@@ -0,0 +1,43 @@
+var faker = require("faker");
+const ghostUrl = Cypress.config("ghostUrl");
+const ghostAuthUrl = Cypress.config("ghostAuthUrl");
+const email = Cypress.config("email");
+const password = Cypress.config("password");
+
+describe("Test - list posts in Ghost", () => {
+  beforeEach(() => {
+    cy.login().then((user) => {
+      cy.visit(ghostUrl);
+      cy.wait(2000);
+    });
+  });
+  it("List Posts", () => {
+
+    cy.get(".gh-nav-top").contains("Posts").click();
+    cy.wait(2000);
+    cy.get(".gh-canvas-title").should("be.visible");
+
+  });
+});
+
+Cypress.Commands.add("login", () => {
+  // 1. Send log in API call
+  return cy
+    .request({
+      url: ghostAuthUrl,
+      method: "POST",
+      headers: {
+        Referer: baseUrl,
+      },
+      body: {
+        username: email,
+        password: password,
+      },
+    })
+    .then(({ body }) => {
+      console.log(body);
+      // Save received auth token to local storage
+      window.localStorage.setItem("authToken", body.authToken);
+      return body.data;
+    });
+});
